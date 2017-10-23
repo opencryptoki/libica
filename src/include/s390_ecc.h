9f0a3a00d92b6062b084c29774e49cb8d7951085
Joerg Schmidbauer <jschmidb@de.ibm.com>
2017-10-23 14:53:04 +0200

    ECC support for Libica.
    Signed-off-by: Joerg Schmidbauer <jschmidb@de.ibm.com>

---
+++ src/include/s390_ecc.h
@@ -0,0 +1,434 @@
+/* This program is released under the Common Public License V1.0
+ *
+ * You should have received a copy of Common Public License V1.0 along with
+ * with this program.
+ */
+
+/**
+ * Authors: Joerg Schmidbauer <jschmidb@de.ibm.com>
+ *
+ * Copyright IBM Corp. 2017
+ */
+
+#ifndef S390_ECDH_H
+#define S390_ECDH_H
+
+#include <openssl/bn.h>
+#include <openssl/obj_mac.h>
+#include <asm/zcrypt.h>
+#include "ica_api.h"
+
+#define MAX_ECC_PRIV_SIZE	66 /* 521 bits */
+#define MAX_ECDSA_SIG_SIZE	132
+
+#define CURVE_TYPE_PRIME		0x00
+#define CURVE_TYPE_BRAINPOOL	0x01
+
+struct ec_key_t {
+	uint32_t nid;
+	unsigned int key_generated;
+	unsigned char* X;
+	unsigned char* Y;
+	unsigned char* D;
+}; /* ICA_EC_KEY */
+
+/**
+ * CCA token header.
+ */
+typedef struct {
+	uint8_t tkn_hdr_id;
+	uint8_t tkn_hdr_version;
+	uint16_t tkn_length;
+	uint8_t reserved[4];
+} __attribute__((packed)) CCA_TOKEN_HDR;
+
+/**
+ * ECC private key section
+ */
+typedef struct {
+	uint8_t section_id; /* 0x20 = ecc private key */
+	uint8_t version;
+	uint16_t section_len;
+	uint8_t wrapping_method;
+	uint8_t hash_used_for_wrapping;
+	uint8_t reserved1[2];
+	uint8_t key_usage;
+	uint8_t curve_type; /* 0x00 = prime, 0x01 = brainpool */
+	uint8_t key_format;
+	uint8_t reserved2;
+	uint16_t priv_p_bitlen; /* length of prime p in bits */
+	uint16_t ibm_associated_data_len;
+	uint8_t kvp[8];
+	uint8_t obj_protection_key[48];
+	uint16_t associated_data_len;
+	uint16_t formatted_data_len;
+} __attribute__((packed)) ECC_PRIVATE_KEY_SECTION;
+
+/**
+ * ECC associated data.
+ */
+typedef struct {
+	uint8_t version;
+	uint8_t key_label;
+	uint16_t ibm_data_len;
+	uint16_t ibm_ext_ad_len;
+	uint8_t user_def_ad_len;
+	uint8_t curve_type;
+	uint16_t p_bitlen;
+	uint8_t usage_flag;
+	uint8_t format_and_sec_flag;
+	uint8_t reserved[4];
+} __attribute__((packed)) ECC_ASSOCIATED_DATA;
+
+/**
+ * ECC public key section.
+ */
+typedef struct {
+	uint8_t section_id; /* 0x21 = ecc public key */
+	uint8_t version;
+	uint16_t section_len;
+	uint8_t reserved1[4];
+	uint8_t curve_type;
+	uint8_t reserved2;
+	uint16_t pub_p_bitlen;
+	uint16_t pub_q_bytelen;
+} __attribute__((packed)) ECC_PUBLIC_KEY_SECTION;
+
+/**
+ * ECC private key token
+ */
+typedef struct {
+	uint16_t key_len;
+	uint16_t reserved;
+	CCA_TOKEN_HDR tknhdr;
+	ECC_PRIVATE_KEY_SECTION privsec;
+	ECC_ASSOCIATED_DATA adata;
+	unsigned char privkey[0];
+	/* here comes the variable length private key (D) */
+} __attribute__((packed)) ECC_PRIVATE_KEY_TOKEN;
+
+/**
+ *  ECC public key token
+ */
+typedef struct {
+	ECC_PUBLIC_KEY_SECTION pubsec;
+	uint8_t compress_flag;
+	unsigned char pubkey[0];
+	/* here comes the variable length public key (X,Y) */
+} __attribute__((packed)) ECC_PUBLIC_KEY_TOKEN;
+
+/**
+ * ECC keyblock, just the length field.
+ */
+typedef struct {
+	uint16_t keyblock_len;
+} __attribute__((packed)) ECC_KEYBLOCK_LENGTH;
+
+/**
+ * A null key token.
+ */
+typedef struct {
+	uint16_t nullkey_len;
+	uint8_t nkey[66];
+} ECDH_NULLKEY;
+
+/**
+ * An ecc nullkey block.
+ */
+typedef struct {
+	uint16_t len;
+	uint16_t flags;
+	uint8_t nulltoken;
+} ECC_NULL_TOKEN;
+
+/**
+ * ECDH parmblock.
+ */
+typedef struct {
+	uint16_t subfunc_code;
+	struct {
+		uint16_t rule_array_len;
+		uint8_t rule_array_cmd[8];
+	} rule_array;
+	struct {
+		uint16_t vud_len;
+		uint8_t vud1[4];
+		uint8_t vud2[6];
+		uint8_t vud3[4];
+		uint8_t vud4[4];
+	} vud_data;
+} __attribute__((packed)) ECDH_PARMBLOCK;
+
+/**
+ * ECDH reply
+ */
+typedef struct {
+	uint8_t reply_cprbx[sizeof(struct CPRBX)];
+	uint8_t subfunc_code[2];
+	uint16_t rule_len;
+	uint8_t vud[14];
+	uint16_t key_block_len;
+	uint16_t key_len; /* keylen-4 is the z-value length */
+	uint16_t key_tag;
+	uint8_t raw_z_value[MAX_ECC_PRIV_SIZE];
+} __attribute__((packed)) ECDH_REPLY;
+
+unsigned int ecdh_hw(ica_adapter_handle_t adapter_handle,
+		const ICA_EC_KEY *privkey_A, const ICA_EC_KEY *pubkey_B,
+		unsigned char *z);
+
+unsigned int ecdh_sw(const ICA_EC_KEY *privkey_A,
+		const ICA_EC_KEY *pubkey_B, unsigned char *z);
+
+ECDH_REPLY* make_ecdh_request(const ICA_EC_KEY *privkey_A,
+		const ICA_EC_KEY *pubkey_B, struct ica_xcRB* xcrb);
+
+/**
+ * ECDSA parmblock.
+ */
+typedef struct {
+	uint16_t subfunc_code;
+	struct {
+		uint16_t rule_array_len;
+		uint8_t rule_array_cmd[8];
+	} rule_array;
+	struct {
+		uint16_t vud_len;
+		uint16_t vud1_len;
+		uint8_t vud1[0];
+		/* Here comes the variable length data to sign/verify */
+	} vud_data;
+} __attribute__((packed)) ECDSA_PARMBLOCK_PART1;
+
+typedef struct {
+	struct {
+		uint16_t vud2_len;
+		uint16_t vud2_data[0];
+		/* Here comes the variable length signature to verify */
+	} vud_data;
+} __attribute__((packed)) ECDSA_PARMBLOCK_PART2;
+
+/**
+ * ECDSA verify public key block
+ */
+typedef struct {
+	uint16_t key_len;
+	uint8_t reserved[2];
+	CCA_TOKEN_HDR tknhdr;
+	ECC_PUBLIC_KEY_SECTION pubsec;
+	uint8_t compress_flag;
+	unsigned char pubkey[0];
+	/* here comes the variable length public key (X,Y) */
+} __attribute__((packed)) ECDSA_PUBLIC_KEY_BLOCK;
+
+/**
+ * ECDSA sign reply
+ */
+typedef struct {
+	uint8_t reply_cprbx[sizeof(struct CPRBX)];
+	uint8_t subfunc_code[2];
+	uint16_t rule_len;
+	uint16_t vud_len;
+	uint8_t vud1[6];
+	uint8_t signature[MAX_ECDSA_SIG_SIZE]; /* siglen = vud_len - 6 - 2 */
+} __attribute__((packed)) ECDSA_SIGN_REPLY;
+
+/**
+ * ECDSA verify reply
+ */
+typedef struct {
+	uint8_t reply_cprbx[sizeof(struct CPRBX)];
+	uint8_t subfunc_code[2];
+	uint16_t rule_len;
+	uint16_t vud_len;
+	uint16_t keylen;
+} __attribute__((packed)) ECDSA_VERIFY_REPLY;
+
+ECDSA_SIGN_REPLY* make_ecdsa_sign_request(const ICA_EC_KEY *privkey,
+		const unsigned char *hash, unsigned int hash_length,
+		struct ica_xcRB* xcrb);
+
+ECDSA_VERIFY_REPLY* make_ecdsa_verify_request(const ICA_EC_KEY *pubkey,
+		const unsigned char *hash, unsigned int hash_length,
+		const unsigned char *signature, struct ica_xcRB* xcrb);
+
+unsigned int ecdsa_sign_hw(ica_adapter_handle_t adapter_handle,
+		const ICA_EC_KEY *privkey, const unsigned char *hash, unsigned int hash_length,
+		unsigned char *signature);
+
+unsigned int ecdsa_sign_sw(const ICA_EC_KEY *privkey,
+		const unsigned char *hash, unsigned int hash_length,
+		unsigned char *signature);
+
+unsigned int ecdsa_verify_hw(ica_adapter_handle_t adapter_handle,
+		const ICA_EC_KEY *pubkey, const unsigned char *hash, unsigned int hash_length,
+		const unsigned char *signature);
+
+unsigned int ecdsa_verify_sw(const ICA_EC_KEY *pubkey,
+		const unsigned char *hash, unsigned int hash_length,
+		const unsigned char *signature);
+
+/**
+ * ECKeyGen parmblock.
+ */
+typedef struct {
+	uint16_t subfunc_code;
+	struct {
+		uint16_t rule_array_len;
+		uint8_t rule_array_cmd[8];
+	} rule_array;
+	uint16_t vud_len; /* no data, only len field */
+} __attribute__((packed)) ECKEYGEN_PARMBLOCK;
+
+/**
+ * ECKeyGen private key struct
+ */
+typedef struct {
+	uint16_t key_len;
+	uint16_t reserved1;
+	CCA_TOKEN_HDR tknhdr;
+	ECC_PRIVATE_KEY_SECTION privsec;
+	ECC_ASSOCIATED_DATA adata;
+	ECC_PUBLIC_KEY_SECTION pubsec;
+} __attribute__((packed)) ECKEYGEN_KEY_TOKEN;
+
+/**
+ * ECKeyGen reply
+ */
+typedef struct {
+	uint8_t reply_cprbx[sizeof(struct CPRBX)];
+	uint8_t subfunc_code[2];
+	uint16_t rule_len;
+	uint16_t vud_len;
+	uint16_t keyblock_len;
+	ECC_PRIVATE_KEY_TOKEN eckey;
+} __attribute__((packed)) ECKEYGEN_REPLY;
+
+unsigned int eckeygen_hw(ica_adapter_handle_t adapter_handle, ICA_EC_KEY *key);
+
+unsigned int eckeygen_sw(ICA_EC_KEY *key);
+
+ECKEYGEN_REPLY* make_eckeygen_request(ICA_EC_KEY *key, struct ica_xcRB* xcrb);
+
+/**
+ * returns 1 if the given data length is valid for Crypto Express, 0 otherwise.
+ */
+static inline int hash_length_valid(unsigned int length)
+{
+	switch (length) {
+	case 20:
+	case 28:
+	case 32:
+	case 48:
+	case 64:
+		return 1;
+	default:
+		return 0;
+	}
+}
+
+/**
+ * returns 1 if the curve specified by nid is supported, 0 otherwise.
+ */
+static inline int curve_supported(unsigned int nid)
+{
+	switch (nid) {
+	case NID_X9_62_prime192v1:
+	case NID_secp224r1:
+	case NID_X9_62_prime256v1:
+	case NID_secp384r1:
+	case NID_secp521r1:
+	case NID_brainpoolP160r1:
+	case NID_brainpoolP192r1:
+	case NID_brainpoolP224r1:
+	case NID_brainpoolP256r1:
+	case NID_brainpoolP320r1:
+	case NID_brainpoolP384r1:
+	case NID_brainpoolP512r1:
+		return 1;
+	default:
+		return 0;
+	}
+}
+
+/**
+ * returns the curve type (prime or brainpool) for the given nid.
+ * returns -1 for any unknown nid.
+ */
+static inline short curve_type_from_nid(unsigned int nid)
+{
+	switch (nid) {
+	case NID_X9_62_prime192v1:
+	case NID_secp224r1:
+	case NID_X9_62_prime256v1:
+	case NID_secp384r1:
+	case NID_secp521r1:
+		return CURVE_TYPE_PRIME;
+	case NID_brainpoolP160r1:
+	case NID_brainpoolP192r1:
+	case NID_brainpoolP224r1:
+	case NID_brainpoolP256r1:
+	case NID_brainpoolP320r1:
+	case NID_brainpoolP384r1:
+	case NID_brainpoolP512r1:
+		return CURVE_TYPE_BRAINPOOL;
+	default:
+		return -1;
+	}
+}
+
+/**
+ * returns the length in bytes of the EC private key D-value
+ * for the given nid.
+ * returns -1 for any unknown nid.
+ */
+static inline int privlen_from_nid(unsigned int nid)
+{
+	switch (nid) {
+	case NID_brainpoolP160r1:
+		return 20;
+	case NID_X9_62_prime192v1:
+	case NID_brainpoolP192r1:
+		return 24;
+	case NID_secp224r1:
+	case NID_brainpoolP224r1:
+		return 28;
+	case NID_X9_62_prime256v1:
+	case NID_brainpoolP256r1:
+		return 32;
+	case NID_brainpoolP320r1:
+		return 40;
+	case NID_secp384r1:
+	case NID_brainpoolP384r1:
+		return 48;
+	case NID_brainpoolP512r1:
+		return 64;
+	case NID_secp521r1:
+		return 66;
+	default:
+		return -1;
+	}
+}
+
+static inline unsigned int getenv_icapath()
+{
+	char* s = getenv("ICAPATH");
+	int icapath=0; /* hw with sw fallback (default) */
+	int env_icapath;
+
+	if (s) {
+#ifdef ICA_DEBUG
+		printf("icapath from getenv: [%s] \n", s);
+#endif
+		if (sscanf(s, "%d", &env_icapath) == 1) {
+			switch (env_icapath) {
+				case 1:	return 1; /* sw only */
+				case 2: return 2; /* hw only */
+			}
+		}
+	}
+
+	return icapath;
+}
+
+#endif
---
+++ src/tests/libica_eckeygen_test.c
@@ -0,0 +1,128 @@
+#include <errno.h>
+#include <openssl/crypto.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+#include <openssl/opensslconf.h>
+#ifdef OPENSSL_FIPS
+#include <openssl/fips.h>
+#endif /* OPENSSL_FIPS */
+
+#include "ica_api.h"
+#include "testcase.h"
+#include <openssl/obj_mac.h>
+
+
+#define MAX_ECC_PRIV_SIZE		66 /* 521 bits */
+#define MAX_ECDSA_SIG_SIZE		2*MAX_ECC_PRIV_SIZE
+#define NUM_ECKEYGEN_TESTS		(sizeof(eckeygen_tests)/sizeof(eckeygen_test_t))
+#define NUM_HASH_LENGTHS		(sizeof(hash_length)/sizeof(int))
+
+typedef struct {
+    unsigned int nid;
+    char nid_str[32];
+} eckeygen_test_t;
+
+static eckeygen_test_t eckeygen_tests[] = {
+	{NID_X9_62_prime192v1, "NID_X9_62_prime192v1"},
+	{NID_secp224r1, "NID_secp224r1"},
+	{NID_X9_62_prime256v1, "NID_X9_62_prime256v1"},
+	{NID_secp384r1, "NID_secp384r1"},
+	{NID_secp521r1, "NID_secp521r1"},
+	{NID_brainpoolP160r1, "NID_brainpoolP160r1"},
+	{NID_brainpoolP192r1, "NID_brainpoolP192r1"},
+	{NID_brainpoolP224r1, "NID_brainpoolP224r1"},
+	{NID_brainpoolP256r1, "NID_brainpoolP256r1"},
+	{NID_brainpoolP320r1, "NID_brainpoolP320r1"},
+	{NID_brainpoolP384r1, "NID_brainpoolP384r1"},
+	{NID_brainpoolP512r1, "NID_brainpoolP512r1"},
+};
+
+static unsigned char hash[] = {
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+	0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+	0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+	0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
+	0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+	0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+	0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+	0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
+};
+
+static unsigned int hash_length[] = {
+	20, 28, 32, 48, 64,
+};
+
+
+int main(int argc, char **argv)
+{
+	ica_adapter_handle_t adapter_handle;
+	unsigned int i, j, rc;
+	unsigned int errors=0;
+	unsigned char signature[MAX_ECDSA_SIG_SIZE];
+	unsigned char pub_X[MAX_ECC_PRIV_SIZE];
+	unsigned char pub_Y[MAX_ECC_PRIV_SIZE];
+	unsigned char priv_D[MAX_ECC_PRIV_SIZE];
+	unsigned int privlen = 0;
+	ICA_EC_KEY *eckey;
+
+	set_verbosity(argc, argv);
+
+	rc = ica_open_adapter(&adapter_handle);
+	if (rc != 0) {
+		V_(printf("ica_open_adapter failed and returned %d (0x%x).\n", rc, rc));
+		return 8;
+	}
+
+	/* Iterate over curves */
+	for (i = 0; i < NUM_ECKEYGEN_TESTS; i++) {
+
+		memset(pub_X, 0, sizeof(pub_X));
+		memset(pub_Y, 0, sizeof(pub_Y));
+		memset(priv_D, 0, sizeof(priv_D));
+
+		V_(printf("Generating EC key for curve %d (%s) \n", eckeygen_tests[i].nid, eckeygen_tests[i].nid_str));
+
+		/* generate EC key with given curve */
+		eckey = ica_ec_key_new(eckeygen_tests[i].nid, &privlen);
+		rc = ica_ec_key_generate(adapter_handle, eckey);
+		if (rc) {
+			V_(printf("EC key for curve %i could not be generated, rc=%i.\n", eckeygen_tests[i].nid, rc));
+			errors++;
+		}
+
+		for (j = 0; j<NUM_HASH_LENGTHS; j++) {
+
+			/* calculate ECDSA signature with this key */
+			rc = ica_ecdsa_sign(adapter_handle, eckey, hash, hash_length[j],
+					signature, MAX_ECDSA_SIG_SIZE);
+
+			if (rc) {
+				V_(printf("Signature could not be created, key not usable, rc=%i.\n",rc));
+				errors++;
+			}
+
+			/* verify ECDSA signature with this key */
+			rc = ica_ecdsa_verify(adapter_handle, eckey, hash, hash_length[j],
+					signature, MAX_ECDSA_SIG_SIZE);
+
+			if (rc) {
+				V_(printf("Signature could not be verified, key not usable, rc=%i.\n",rc));
+				errors++;
+			}
+		}
+
+		ica_ec_key_free(eckey);
+	}
+
+	ica_close_adapter(adapter_handle);
+
+	if (errors)
+		printf("%i of %li EC keygen tests failed.\n", errors, NUM_ECKEYGEN_TESTS);
+	else
+		printf("All EC keygen tests passed.\n");
+
+	return 0;
+}
