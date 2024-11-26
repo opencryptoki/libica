6f5a06ff1236f1ac99966bc11de4dd5335d6f1c0
Joerg Schmidbauer <jschmidb@de.ibm.com>
2024-11-26 10:54:49 +0100

    fips update: move fips lists to libica library
    Move the fips algo lists to fips.c to have them in the libica library
    instance. Applications like icainfo obtain info about algorithms
    being blocked or overridden via the fips indicator.
    
    Signed-off-by: Joerg Schmidbauer <jschmidb@de.ibm.com>

--- src/fips.c
+++ src/fips.c
@@ -1358,4 +1358,56 @@ _err_:
 	syslog(LOG_ERR, "Libica RSA test failed.");
 	return 1;
 }
+
+/*
+ * List of non-fips-approved algorithms
+ */
+const int FIPS_BLACKLIST[] = {DES_ECB, DES_CBC, DES_CBC_CS, DES_OFB,
+	DES_CFB, DES_CTR, DES_CTRLST, DES_CBC_MAC, DES_CMAC, P_RNG, DES3_ECB,
+	DES3_CBC, DES3_CBC_CS, DES3_OFB, DES3_CFB, DES3_CTR, DES3_CTRLST,
+	DES3_CBC_MAC, DES3_CMAC, ED25519_KEYGEN, ED25519_SIGN, ED25519_VERIFY,
+	ED448_KEYGEN, ED448_SIGN, ED448_VERIFY, X25519_KEYGEN, X25519_DERIVE,
+	X448_KEYGEN, X448_DERIVE, RSA_ME, RSA_CRT, SHA512_DRNG };
+const size_t FIPS_BLACKLIST_LEN
+	= sizeof(FIPS_BLACKLIST) / sizeof(FIPS_BLACKLIST[0]);
+
+/*
+ * FIPS service indicator: List of tolerated but non-approved algorithms.
+ */
+const int FIPS_OVERRIDE_LIST[] = { RSA_ME, RSA_CRT, SHA512_DRNG };
+const size_t FIPS_OVERRIDE_LIST_LEN
+	= sizeof(FIPS_OVERRIDE_LIST) / sizeof(FIPS_OVERRIDE_LIST[0]);
+
+/*
+ * Returns 1 if the algorithm identified by @id is FIPS approved.
+ * Returns 0 otherwise.
+ */
+int fips_approved(int id)
+{
+	size_t i;
+
+	for (i = 0; i < FIPS_BLACKLIST_LEN; i++) {
+		if (id == FIPS_BLACKLIST[i])
+			return 0;
+	}
+
+	return 1;
+}
+
+/*
+ * Returns 1 if the algorithm identified by @id is FIPS tolerated, i.e. it is
+ * available via the API in fips mode, but considered non-approved.
+ * Returns 0 otherwise.
+ */
+int fips_override(int id)
+{
+	size_t i;
+
+	for (i = 0; i < FIPS_OVERRIDE_LIST_LEN; i++) {
+		if (id == FIPS_OVERRIDE_LIST[i])
+			return 1;
+	}
+
+	return 0;
+}
 #endif /* FIPS_H */
